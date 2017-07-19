059c3d953f02b18b8e8ca89269c6e52f7520b4b1
Mateo Vidal <jvidalba@gmail.com>
2017-07-19 16:00:44 -0500

    Add user and custom zemoga routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "users#index"
+  resources :users
+
+  scope '/zemoga_portfolio_api' do
+    get 'user_info/:id', to: "users#show"
+    put 'modify_user_info/:id', to: "users#udpate"
+  end
 end
