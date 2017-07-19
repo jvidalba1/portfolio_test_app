380edc5e271b7b34922a214fa95074965d9fd0b6
Mateo Vidal <jvidalba@gmail.com>
2017-07-19 15:49:10 -0500

    User generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  profile_image   :string
+#  name            :string
+#  description     :text
+#  twitter_account :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
   
 end
